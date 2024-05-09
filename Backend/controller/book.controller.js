6b4971b18522e9b82ee3ca84e01d246019d3309f
SKM5 <kadam.sayali@gmail.com>
2024-05-08 20:22:10 -0700

    Search implemented completed

--- Backend/controller/book.controller.js
+++ Backend/controller/book.controller.js
@@ -9,3 +9,14 @@ export const getBook = async(req, res) => {
         res.status(500).json(error);
     }
 };
+
+export const searchBook = async (req, res) => {
+    try {
+        const { query } = req.query;
+        const books = await Book.find({ $text: { $search: query } });
+        res.status(200).json(books);
+    } catch (error) {
+        console.log("Error: ", error);
+        res.status(500).json(error);
+    }
+};
--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from 'react';
 import banner from "../../public/Banner.png";
+import BookSearch from "./BookSearch"; // Import the BookSearch component
+
 function Banner() {
+
+  const [book, setBook] = useState([]);
+
   return (
     <>
       <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row my-10">
@@ -8,10 +13,8 @@ function Banner() {
           <div className="space-y-8">
             <h1 className="text-4xl font-bold">Welcome to <span className="text-pink-500"> Book Share! </span> Share the <span className="text-pink-500"> Magic </span>...</h1> 
             <h1 className="text-4xl font-bold">Explore the World of Books!!</h1>
-            <label className="input input-bordered flex items-center gap-2">
-                <input type="text" className="grow" placeholder="Search" />
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
-            </label>
+           {/* Include the BookSearch component here */}
+           <BookSearch setBook={setBook} />
           </div>
           <button className="btn mt-6 btn-secondary">Explore</button>
         </div>
